a63b6d0aefc3113e2173f97ca19d589169c972b0
Daniel Q. <danielquesadadev@gmail.com>
2024-04-16 16:21:59 +0200

    feat: first commit, rutes finished

---
+++ src/app/shared/heavy-loaders/users-loaders.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: `<h1>Hola mundo</h1>`,
+})
+export class UsersLoadersComponent {
+  constructor() {}
+}
